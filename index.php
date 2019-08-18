5708428f876e7b25529d46398f6454c9cfc83d7c
idawnlight <idawn@live.com>
2019-08-18 10:32:04 +0800

    update(version): release 0.1.1

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ const CORE_DIR = ROOT_DIR  . 'core/';
 const SERVICE_DIR = ROOT_DIR  . 'service/';
 const CACHE_DIR = ROOT_DIR . 'cache/';
 const GIT_DIR = ROOT_DIR . '.git/';
-const VERSION = '0.1.0';
+const VERSION = '0.1.1';
 
 require_once ROOT_DIR . 'vendor/autoload.php';
 
