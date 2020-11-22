37ad061356f16caad785789bf9653e5a176bba0a
idawnlight <idawn@live.com>
2020-11-22 23:02:30 +0800

    fix(service): auto redirect of custom qq email
    e.g. nexmoe@qq.com

--- service/Auto/Action.php
+++ service/Auto/Action.php
@@ -5,16 +5,22 @@ namespace Service\Auto;
 use Core\Components\Config;
 use Core\Components\Helper;
 use Core\Contracts\Service\ActionAbstract;
+use Exception;
 use Service\Tencent\Lib;
 
 class Action extends ActionAbstract
 {
     public function index() {
         $this->para = Lib::parseData($this->para);
-        if (filter_var($this->para['identifier'], FILTER_VALIDATE_EMAIL) && strpos($this->para['identifier'], "@qq.com")) {
-            $qq = explode("@qq.com", $this->para['identifier'])[0];
-            $url = Config::domain() . 'qq/' . $qq . '?s=' . $this->para['size'];
-        } else {
+        try {
+            if (filter_var($this->para['identifier'], FILTER_VALIDATE_EMAIL) && strpos($this->para['identifier'], "@qq.com")) {
+                $qq = explode("@qq.com", $this->para['identifier'])[0];
+                if (!is_numeric($qq)) throw new Exception();
+                $url = Config::domain() . 'qq/' . $qq . '?s=' . $this->para['size'];
+            } else {
+                throw new Exception();
+            }
+        } catch (Exception $e) {
             $url = Config::domain() . 'gravatar/'. md5($this->para['identifier']) . "?s=" . $this->para['size'];
         }
         $this->handler->response(Helper::createRedirectResponse($url), $this->responseId);
